e19804f3e5707152ec7d473d8faa944eed78ff63
Lauri Himanen <lauri.himanen@aalto.fi>
2019-11-07 08:37:09 +0200

    Connected sliders to visualization.

--- js/index.js
+++ js/index.js
@@ -23,7 +23,7 @@ let options = {
     enablePan: false,
     enableRotate: true,
     translation: [2.5, 2.5, 2.5],
-    zoomLevel: 0.65
+    zoomLevel: 0.6
 };
 var viewer = new structureviewer.StructureViewer(targetElem, false, options);
 viewer.loadJSON("data/geometry.json");
@@ -156,61 +156,93 @@ function rotater() {
     rotated11(1);
 }
 
-setInterval(rotater, 50);
+//setInterval(rotater, 50);
 
 // Connect linear sliders to viz
 let d11 = document.getElementById("d11");
-d11.onchange = function() {
-    d11val.innerHTML = this.value;
-}
+var d11angle = 0;
+d11.oninput = function() {
+    let angle = this.value;
+    let newangle = angle-d11angle;
+    rotated11(newangle);
+    console.log(angle, newangle);
+    d11angle = angle;
+};
+let d13 = document.getElementById("d13");
+var d13angle = 0;
+d13.oninput = function() {
+    let angle = this.value;
+    let newangle = angle-d13angle;
+    rotated13(newangle);
+    console.log(angle, newangle);
+    d13angle = angle;
+};
+let d7 = document.getElementById("d7");
+var d7angle = 0;
+d7.oninput = function() {
+    let angle = this.value;
+    let newangle = angle-d7angle;
+    rotated7(newangle);
+    console.log(angle, newangle);
+    d7angle = angle;
+};
+let d4 = document.getElementById("d4");
+var d4angle = 0;
+d4.oninput = function() {
+    let angle = this.value;
+    let newangle = angle-d4angle;
+    rotated4(newangle);
+    console.log(angle, newangle);
+    d4angle = angle;
+};
 
 
 //==============================================================================
 // For setting up the radial sliders
-var sliders = document.getElementsByClassName("round-slider");
-for (let i = 0; i < sliders.length; i++) {
-	sliders[i].addEventListener("click", round_slider_tune, false);
-	sliders[i].addEventListener("mousedown", function(event) {
-		sliders[i].onmousemove = function(event) {
-			if (event.buttons == 1 || event.buttons == 3) {
-				round_slider_tune(event);
-			}
-		}
-	});
-}
+//var sliders = document.getElementsByClassName("round-slider");
+//for (let i = 0; i < sliders.length; i++) {
+	//sliders[i].addEventListener("click", round_slider_tune, false);
+	//sliders[i].addEventListener("mousedown", function(event) {
+		//sliders[i].onmousemove = function(event) {
+			//if (event.buttons == 1 || event.buttons == 3) {
+				//round_slider_tune(event);
+			//}
+		//}
+	//});
+//}
 
-function round_slider_tune(event) {
-	let eventDoc = (event.target && event.target.ownerDocument) || document,
-		doc = eventDoc.documentElement,
-		body = eventDoc.body;
-	event.pageX = event.clientX +
-		  (doc && doc.scrollLeft || body && body.scrollLeft || 0) -
-		  (doc && doc.clientLeft || body && body.clientLeft || 0);
-	event.pageY = event.clientY +
-		  (doc && doc.scrollTop  || body && body.scrollTop  || 0) -
-		  (doc && doc.clientTop  || body && body.clientTop  || 0 );
-	let output = event.target.getElementsByClassName("selection")[0],
-		text = event.target.getElementsByClassName("holder")[0],
-		styleafter = document.head.appendChild(document.createElement("style")),
-		elpos = event.target.getBoundingClientRect(),
-		cX = elpos.width / 2,
-		cY = elpos.height / 2,
-		eX = event.pageX - elpos.left,
-		eY = event.pageY - elpos.top,
-		dX = 0,
-		dY = 0,
-		angle = Math.atan2(cX - eX, cY - eY) * (180 / Math.PI),
-		value = 100;
+//function round_slider_tune(event) {
+	//let eventDoc = (event.target && event.target.ownerDocument) || document,
+		//doc = eventDoc.documentElement,
+		//body = eventDoc.body;
+	//event.pageX = event.clientX +
+		  //(doc && doc.scrollLeft || body && body.scrollLeft || 0) -
+		  //(doc && doc.clientLeft || body && body.clientLeft || 0);
+	//event.pageY = event.clientY +
+		  //(doc && doc.scrollTop  || body && body.scrollTop  || 0) -
+		  //(doc && doc.clientTop  || body && body.clientTop  || 0 );
+	//let output = event.target.getElementsByClassName("selection")[0],
+		//text = event.target.getElementsByClassName("holder")[0],
+		//styleafter = document.head.appendChild(document.createElement("style")),
+		//elpos = event.target.getBoundingClientRect(),
+		//cX = elpos.width / 2,
+		//cY = elpos.height / 2,
+		//eX = event.pageX - elpos.left,
+		//eY = event.pageY - elpos.top,
+		//dX = 0,
+		//dY = 0,
+		//angle = Math.atan2(cX - eX, cY - eY) * (180 / Math.PI),
+		//value = 100;
 
-	if (Math.abs(eX - cX) >= Math.abs(eY - cY)) { // 110 90
-		dX = 150 / 2 + Math.sign(eX - cX) * 150 / 2;
-		dY = 150 / 2 + (eY - cY) / Math.abs(eX - cX) * 150 / 2;
-	} else {
-		dX = 150 / 2 + (eX - cX) / Math.abs(eY - cY) * 150 / 2;
-		dY = 150 / 2 + Math.sign(eY - cY) * 150 / 2;
-	}
-	dX = Math.round(dX / 150 * 100)
-	dY = Math.round(dY / 150 * 100)
-	styleafter.innerHTML = ".round-slider {transform: rotate(" + -angle + "deg);}";
-	text.innerHTML = value + "%";
-}
+	//if (Math.abs(eX - cX) >= Math.abs(eY - cY)) { // 110 90
+		//dX = 150 / 2 + Math.sign(eX - cX) * 150 / 2;
+		//dY = 150 / 2 + (eY - cY) / Math.abs(eX - cX) * 150 / 2;
+	//} else {
+		//dX = 150 / 2 + (eX - cX) / Math.abs(eY - cY) * 150 / 2;
+		//dY = 150 / 2 + Math.sign(eY - cY) * 150 / 2;
+	//}
+	//dX = Math.round(dX / 150 * 100)
+	//dY = Math.round(dY / 150 * 100)
+	//styleafter.innerHTML = ".round-slider {transform: rotate(" + -angle + "deg);}";
+	//text.innerHTML = value + "%";
+//}
